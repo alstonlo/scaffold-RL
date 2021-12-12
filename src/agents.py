c1b873d0c54b1d3341c2d328115c57a16845ad87
alstonlo <40709307+alstonlo@users.noreply.github.com>
2021-12-12 10:25:17 -0500

    refactor to clean up code structure

--- src/agents.py
+++ src/agents.py
@@ -1,25 +1,10 @@
 import random
 
 
-def greedy_choice(action_space, metric):
-    best = (None, float("-inf"))
-    for action in action_space:
-        score = metric(action)
-        if score > best[1]:
-            best = (action, score)
-    return best[0]
-
-
 class Agent:
 
-    def rollout(self, env):
+    def sample_action(self, env):
         raise NotImplementedError()
-
-
-class EpsilonGreedyAgent(Agent):
-
-    def __init__(self, epsilon):
-        self.epsilon = epsilon
 
     def rollout(self, env):
         env.reset()
@@ -27,11 +12,26 @@ class EpsilonGreedyAgent(Agent):
         obs = env.state
         value = 0.0
         for _ in range(env.max_steps):
-            action_space = env.valid_actions
-            if random.random() < self.epsilon:
-                act = random.choice(action_space)
-            else:
-                act = greedy_choice(action_space, env.prop_fn)
+            act = self.sample_action(env)
             obs, reward, _ = env.step(act)
             value += reward
         return obs[0], value
+
+
+class EpsilonGreedyAgent(Agent):
+
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+
+    def sample_action(self, env):
+        action_space = env.valid_actions
+
+        if random.random() < self.epsilon:
+            return random.choice(action_space)
+        else:
+            best = (None, float("-inf"))
+            for action in action_space:
+                score = env.prop_fn(action)
+                if score > best[1]:
+                    best = (action, score)
+            return best[0]
